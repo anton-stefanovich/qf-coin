739f255c973467ef0a17e4a0dbd364e05f5e6b7b
Anton Stefanovich <anton.stefanovich@gmail.com>
2021-02-13 16:30:42 -0500

    #12 Cron and daemon starts has been separated

--- __init__.py
+++
@@ -1,7 +0,0 @@
-
-if __name__ == '__main__':
-    from tools.config import Config
-    config = Config.read('config.json')
-
-    from core.trader import Trader
-    Trader(config).go()
---
+++ cron.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    from core import start
+    start(once=True)
