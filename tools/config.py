33bc5640cc2e41407f70acb45e87f461900528eb
Anton Stefanovich <anton.stefanovich@gmail.com>
2021-02-13 14:27:11 -0500

    #8 The  issue with boolean arguments parsing has been fixed

--- tools/config.py
+++ tools/config.py
@@ -11,15 +11,22 @@ class Config:
         from argparse import ArgumentParser
         parser = ArgumentParser('Auto''trader by QF')
 
+        def get_aliases(key: str) -> tuple:
+            return '--%s' % key.replace('-', '_'), f'--{key}'
+
         for _type, _keys in {
-            bool:  ('debug-mode', ),
             float: ('debug-exchange-fee', 'debug-amount',
                     'trade-limit', 'trade-rebound', 'trade-timeout'),
             str:   ('trade-currencies', 'trade-cookie', 'debug-scope',
                     'api-key', 'api-secret', 'api-version')}.items():
             for _key in _keys:  # iterating the keys set
-                parser.add_argument(
-                    f'--%s' % _key.replace('-', '_'), f'--{_key}',
-                    type=_type, default=config.get(_key, None))
+                parser.add_argument(*get_aliases(_key), type=_type,
+                                    default=config.get(_key, None))
+
+        for _key, _default in {
+                'debug-mode': True}.items():
+            from tools.converter import any_to_bool
+            parser.add_argument(*get_aliases(_key), type=any_to_bool, nargs='?',
+                                const=True, default=config.get(_key, _default))
 
         return parser.parse_args()
